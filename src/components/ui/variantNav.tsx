9243f43a626630c366d5e8c7da5acd611eaa9afb
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-08 06:03:19 +0700

    feat: change font weight

--- src/components/ui/variantNav.tsx
+++ src/components/ui/variantNav.tsx
@@ -5,27 +5,25 @@ import { Heart20SolidIcon } from "../icons/heroicons-heart-20-solid";
 
 function VariantNav() {
   return (
-    <div>
-      <div className="my-5 flex items-center justify-center gap-10">
-        <Link to={"#"} className="flex flex-col items-center gap-1">
-          <div className="rounded-full bg-gray-200 p-3">
-            <Star20SolidIcon fill="" />
-          </div>
-          <span>Grade A</span>
-        </Link>
-        <Link to={"#"} className="flex flex-col items-center gap-1">
-          <div className="rounded-full bg-gray-200 p-3">
-            <Trophy16SolidIcon />
-          </div>
-          <span>Comp Grade</span>
-        </Link>
-        <Link to={"#"} className="flex flex-col items-center gap-1">
-          <div className="rounded-full bg-gray-200 p-3">
-            <Heart20SolidIcon />
-          </div>
-          <span>Bredding</span>
-        </Link>
-      </div>
+    <div className="m-3 flex items-center justify-center gap-10 rounded-2xl py-2">
+      <Link to={"#"} className="flex flex-col items-center gap-1">
+        <div className="rounded-full bg-gray-100 p-3">
+          <Star20SolidIcon fill="#EE0B0B" stroke="none" />
+        </div>
+        <span>Grade A</span>
+      </Link>
+      <Link to={"#"} className="flex flex-col items-center gap-1">
+        <div className="rounded-full bg-gray-100 p-3">
+          <Trophy16SolidIcon fill="#F3E008" stroke="none" />
+        </div>
+        <span>Comp Grade</span>
+      </Link>
+      <Link to={"#"} className="flex flex-col items-center gap-1">
+        <div className="rounded-full bg-gray-100 p-3">
+          <Heart20SolidIcon fill="#F800A9" stroke="none" />
+        </div>
+        <span>Bredding</span>
+      </Link>
     </div>
   );
 }
