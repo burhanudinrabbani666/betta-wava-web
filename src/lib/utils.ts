b5929bbb2376dee6c85f15c170cf0d4f4993dcbc
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-11 16:38:53 +0000

    feat: implement image

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,16 +8,19 @@ export function cn(...inputs: ClassValue[]) {
 export const variant = [
   {
     title: "Competition Grade",
+    image: "comp-grade",
     description:
       "Show-quality betta with outstanding color, form, and symmetry.",
   },
   {
     title: "Grade A",
+    image: "grade-a",
     description:
       "Beautiful and healthy betta, perfect for collectors and hobbyists.",
   },
   {
     title: "Breeding",
+    image: "breeding",
     description:
       "Strong genetics betta, perfect for breeding and line development.",
   },
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -5,6 +5,7 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { variant } from "@/lib/utils";
+import { Link } from "react-router";
 
 export default function Homepage() {
   return (
@@ -19,11 +20,31 @@ export default function Homepage() {
       <CarouselContent className="h-screen">
         {variant.map((v, index) => (
           <CarouselItem key={index} className="h-screen basis-full px-2">
-            <Card className="h-full">
-              <CardContent className="flex h-full flex-col justify-end">
-                <div className="mb-28 space-y-2">
-                  <p className="text-xl font-normal">{v.title}</p>
-                  <p className="text-sm font-light">{v.description}</p>
+            <Card className="h-full py-0">
+              <CardContent className="h-full px-0">
+                <div
+                  className="flex h-full flex-col justify-end bg-cover bg-top p-6"
+                  style={{
+                    backgroundImage: `url(/variant/${v.image}.jpg)`,
+                  }}
+                >
+                  <Card className="mb-10">
+                    <CardContent>
+                      <div className="mb-4 space-y-1">
+                        <p className="text-4xl font-normal tracking-tighter">
+                          {v.title}
+                        </p>
+                        <p className="text-sm font-light">{v.description}</p>
+                      </div>
+
+                      <Link
+                        to={`/variant/${v.title.toLowerCase().replace(" ", "-")}`}
+                        className="border-b border-neutral-900 py-1"
+                      >
+                        Explore Collection &rarr;
+                      </Link>
+                    </CardContent>
+                  </Card>
                 </div>
               </CardContent>
             </Card>
