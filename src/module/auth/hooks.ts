ac2b13e32b4a88bda81672cbf3de13d05b8af6ae
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-08 15:15:05 +0700

    get token

--- src/module/auth/hooks.ts
+++ src/module/auth/hooks.ts
@@ -1,5 +1,6 @@
 import { useCookies } from "react-cookie";
 import type { CookiesValues } from "./type";
+import { $api } from "../common/api";
 
 export function useAuth() {
   const [cookies, setCookies] = useCookies<"token", CookiesValues>([]);
@@ -9,7 +10,35 @@ export function useAuth() {
   }
 
   return {
-    token: cookies,
+    token: cookies.token,
     setToken,
+  };
+}
+
+export function useUser() {
+  const { token } = useAuth();
+
+  const {
+    data: user,
+    isPending,
+    error,
+  } = $api.useQuery("get", "/auth/me", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (!user || error) {
+    return {
+      isAuthenticated: false,
+      user,
+      isPending,
+      error,
+    };
+  }
+
+  return {
+    isAuthenticated: true,
+    user,
+    isPending,
+    error,
   };
 }
